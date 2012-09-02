f552166378ae2063ed46c27acc7195559b35d84c
Chris Christensen <christian.christensen@gmail.com>
2012-09-01 23:43:47 -0500

    Update mysites to more utility functionality.

--- site.pp
+++ site.pp
@@ -1,33 +1,41 @@
-node default {
+class mysites {
   package { 'php5-fpm':
     ensure => present,
   }
-
-  class { 'nginx': }
-  nginx::resource::vhost { 'ec2-23-22-173-34.compute-1.amazonaws.com':
-    location => '~ \.php$',
-    locations => {
-      0 => {
-        'ensure' => present,
-        'location' => '/',
-        'try_files' => ['$uri', '$uri/', '@rewriteapp'],
-      },
-      1 => {
-        'ensure' => present,
-        'location' => '@rewriteapp',
-        'options' => {'rewrite' => '^ /index.php last'},
-      }
-    },
-    ensure   => present,
-    www_root => '/srv/ec2-1',
-    try_files => ['$uri', '$uri/'],
-    fastcgi => '127.0.0.1:9000',
-    fastcgi_script => '$document_root$fastcgi_script_name',
-  }
-
   service { 'php5-fpm':
     ensure => running,
     require => Package['php5-fpm'],
   }
+
+  class { 'nginx': }
+
+  define install ($vhost = 'localhost', $path = '/srv/www') {
+    nginx::resource::vhost { $vhost:
+      location => '~ \.php$',
+      locations => {
+        "$vhost-0" => {
+          'ensure' => present,
+          'location' => '/',
+          'try_files' => ['$uri', '$uri/', '@rewriteapp'],
+        },
+        "$vhost-1" => {
+          'ensure' => present,
+          'location' => '@rewriteapp',
+          'options' => {'rewrite' => '^ /index.php last'},
+        }
+      },
+      ensure   => present,
+      www_root => $path,
+      try_files => ['$uri', '$uri/'],
+      fastcgi => '127.0.0.1:9000',
+      fastcgi_script => '$document_root$fastcgi_script_name',
+    }
+  }
+}
+
+node default {
+  class { 'mysites': }
+  mysites::install { '_': vhost => '_', path => '/srv/ec2-2' }
+  mysites::install { 'ec2-174-129-134-0.compute-1.amazonaws.com': vhost => 'ec2-174-129-134-0.compute-1.amazonaws.com', path => '/srv/ec2-1' }
 }
 
